d5910b9b560efae5dba1bdf7050dcd24cccf5ad3
Marta Búa <marta_fb8@hotmail.com>
2023-03-25 14:34:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -173,11 +173,15 @@ if selected == "Modelo":
 
     with col2:
 
-        st.write("")
-
+        add_vertical_space(1)
+       
         img = f"foto_predict/con{int(con*100)}_sup{int(sup*100)}/foto.jpg"
 
         st.image(img, caption = "Resultados")
+
+    st.markdown("### Ejemplo")
+
+    st.markdown('<div style="text-align: justify;"><p>A continuación puedes ver como trabaja nuestro modelo en los siguientes vídeos de ejemplo.</p></div>', unsafe_allow_html=True)
 
     option = st.selectbox("Escoge lo que quieres ver", ["chicago", "los_angeles", "new_york", "seattle"])
 
